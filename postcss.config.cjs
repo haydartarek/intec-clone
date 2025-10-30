fc0c94a73b0e4f8f1fc811e60dd0851d3a7fbdfd
Haydar <heydertarek2000@gmail.com>
2025-10-30 01:35:37 +0100

    build(css): purge and add minified main.min.css (production)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,12 @@ module.exports = ({ env }) => ({
   map: false,
   plugins: [
     require('autoprefixer')(),
+    env === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./*.html', './intec-clone/*.html'],
+          defaultExtractor: content => content.match(/[^\s"'<>]+/g) || []
+        })
+      : null,
     env === 'production' ? require('cssnano')({ preset: 'default' }) : null
   ].filter(Boolean)
 });
